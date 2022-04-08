7a793aff8a24592cdb885c838d001e341f87de0c
Lodovico Azzini <lodovico.azzini@usi.ch>
2022-04-08 18:04:32 +0200

    reformat config file

--- MNIST/config.py
+++ MNIST/config.py
@@ -1,26 +1,26 @@
-from os.path import join
 import json
+from os.path import join
 
 # Dataset
-EXPECTED_LABEL   = 5
+EXPECTED_LABEL = 5
 
-IMG_SIZE         = 28
-NUM_CLASSES      = 10
-MODEL            = 'models/tf2_model.h5'
+IMG_SIZE = 28
+NUM_CLASSES = 10
+MODEL = 'models/tf2_model.h5'
 BITMAP_THRESHOLD = 0.5
-FEATURES         = ["Bitmaps", "Moves"] #, "Orientation"]
-NUM_CELLS        = 25
+FEATURES = ["Bitmaps", "Moves"]  # , "Orientation"]
+NUM_CELLS = 25
 
 
 def to_json(folder):
     config = {
-        'label': str(EXPECTED_LABEL),
+        'label': EXPECTED_LABEL,
         'image size': 28,
-        'num classes' : 10,
-        'model': str(MODEL),
-        'features': str(FEATURES),
+        'num classes': 10,
+        'model': MODEL,
+        'features': FEATURES,
         'num cells': 25
     }
-    filedest = join(folder, "config.json")
-    with open(filedest, 'w') as f:
-        (json.dump(config, f, sort_keys=True, indent=4))
+    destination = join(folder, "config.json")
+    with open(destination, 'w') as f:
+        json.dump(config, f, sort_keys=True, indent=4)
